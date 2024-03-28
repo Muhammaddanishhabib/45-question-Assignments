1c7f154c16ed9113604245f5109d446c9cc8129a
Muhammaddanishhabib <d4danishchauhan@gmail.com>
2024-03-28 17:04:35 +0500

    first commit

---
+++ Assignment 02/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Assignment 1
+// Storing the person name in a varible
+let personName = "Danish";
+// Printing a messsage to the person
+console.log(`Hello ${personName} would you like to learn some Python today?`);
---
+++ Assignment 02/main.ts
@@ -0,0 +1,14 @@
+// Assignment 2
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+//would you like to learn some Python today?
+
+// Storing the person name in a varible
+
+let personName: String= "Danish";
+
+// Printing a messsage to the person
+
+console.log(`Hello ${personName} would you like to learn some Python today?`)
+
+
+
