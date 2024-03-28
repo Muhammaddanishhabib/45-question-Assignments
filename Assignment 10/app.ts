e86d55e3673cf63b9ec83b4b75750407c3d100f3
Muhammaddanishhabib <d4danishchauhan@gmail.com>
2024-03-28 16:44:34 +0500

    first commit

---
+++ Assignment 10/app.ts
@@ -0,0 +1,7 @@
+// My Name is Muhammad Danish
+// Dated: 27/03/2023
+
+
+// This progarm will Addition
+
+console.log(5+2);
---
+++ Assignment 10/main.ts
@@ -0,0 +1,12 @@
+// Assignment 10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+//just add your name and the current date at the top of each program file. Then write one sentence
+//describing what the program does.
+
+// My Name is Muhammad Danish
+// Dated: 27/03/2023
+
+// This progarm will rum simple code just like Hello world
+
+console.log("Hello World Simple Program");
